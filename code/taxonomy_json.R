d662ae1a431314458212ba317c94ae79e16dd0a3
Matthew Kling <mattkling@gmail.com>
2017-05-11 23:13:24 -0700

    input data prep for park swapping

--- code/taxonomy_json.R
+++ code/taxonomy_json.R
@@ -1,118 +1,118 @@
 
 library(dplyr)
 library(jsonlite)
-#setwd("..")
 setwd("~/documents/inviz/taxorama")
 
-# load data
-park_id = "PORE" #Point Reyes is park of interest
-spp <- read.csv(paste0("processed_data/",park_id,"_data/",park_id,"_species_list.csv"), header=T, stringsAsFactors=F)
-
-f <- spp %>%
-      mutate(species=speciesFixed,
-             category=tolower(category),
-             root="life") %>%
-      distinct() %>%
-      arrange(category, family, species) %>%
-      as.data.frame() 
-
-
-
-# add colors
-colors <- read.csv(paste0("processed_data/",park_id,"_data/",park_id,"_species_colors.csv"), stringsAsFactors=F) %>%
-      mutate(species=binomial) %>% select(-binomial)
-f <- left_join(f, colors)
-f$hex <- f$hex_ecdf # hex_fit or hex_ecdf
-f$hex[is.na(f$hex)] <- "#000000"
-
-d <- f
-d <- filter(d, 
-            total_obs>0, 
-            !is.na(category),
-            nchar(class)>0)
-d <- d[!duplicated(d$speciesFixed),]
-
-# generate list of leaf-level species datasets
-leaves <- lapply(1:nrow(d), function(i) data.frame(level="species",
-                                                   name=d$speciesFixed[i],
-                                                   common=ifelse(!is.na(d$genus.common[i]), 
-                                                                 d$species.common[i], 
-                                                                 "Common name unknown"),
-                                                   inat_id=d$species.inat.ID[i],
-                                                   hex=d$hex[i],
-                                                   nspp=1,
-                                                   n_records=d$total_obs[i],
-                                                   n_users=d$user_obs[i],
-                                                   n_species=1))
-
-# function aggregates taxa list into parent clades
-group_taxa <- function(data, groupings, level, max_col){
-      parents <- as.vector(unique(groupings[,1]))
-      lapply(parents, function(x){
-            
-            kids <- which(groupings[,1]==x)
-            kids <- data[kids]
-            kids <- kids[!sapply(kids, is.null)]
-            
-            weights_spp <- sapply(kids, function(x) x$nspp)
-            weights_obs <- sapply(kids, function(x) x$nspp)
-            
-            color <- sapply(kids, function(x) x$hex) %>% col2rgb()
-            color <- sapply(1:3, function(x) weighted.mean(color[x,], weights_spp))
-            
-            common <- d[d[,level] == x, paste0(level, ".common")][1]
-            if(is.null(common)) common <- x
-            
-            inat_id <- d[d[,level] == x, paste0(level, ".inat.ID")][1]
-            if(is.null(inat_id)) inat_id <- "NA"
-            
-            list(name=x,
-                 common=common,
-                 inat_id=inat_id,
-                 level=level, 
-                 hex=rgb(color[1], color[2], color[3], maxColorValue=max_col), # mcv/255 ratio controls fade to black
-                 nspp=sum(weights_spp),
-                 nobs=sum(weights_obs),
-                 children=kids)
-      })
-}
-
-hierarchies <- list(linnean=c("root", "kingdom", "phylum", "class", "order", "family", "genus", "species"),
-                    simple=c("root", "category", "family", "species"))
-
-for(hierarchy in names(hierarchies)){
-      # construct hierarchy
-      levels <- hierarchies[[hierarchy]]
-      p <- leaves
-      for(level in rev(levels)[2:length(levels)]){
-            child_level <- levels[match(level, levels)+1]
-            groupings <- distinct(d[,c(level, child_level)])
-            groupings <- groupings[apply(groupings, 1, function(x) min(nchar(x))>0),]
-            p <- group_taxa(p, groupings, level,
-                            max_col=switch(hierarchy, "linnean"=400, "simple"=800))
+for(park_id in c("PORE", "GOGA")){
+      
+      # load data
+      spp <- read.csv(paste0("processed_data/", park_id, "_data/", park_id, "_species_list.csv"), header=T, stringsAsFactors=F)
+      
+      f <- spp %>%
+            mutate(species=speciesFixed,
+                   category=tolower(category),
+                   root="life") %>%
+            distinct() %>%
+            arrange(category, family, species) %>%
+            as.data.frame() 
+      
+      
+      
+      # add colors
+      colors <- read.csv(paste0("processed_data/", park_id, "_data/", park_id, "_species_colors.csv"), stringsAsFactors=F) %>%
+            mutate(species=binomial) %>% select(-binomial)
+      f <- left_join(f, colors)
+      f$hex <- f$hex_ecdf # hex_fit or hex_ecdf
+      f$hex[is.na(f$hex)] <- "#000000"
+      
+      d <- f
+      d <- filter(d, 
+                  total_obs>0, 
+                  !is.na(category),
+                  nchar(class)>0)
+      d <- d[!duplicated(d$speciesFixed),]
+      
+      # generate list of leaf-level species datasets
+      leaves <- lapply(1:nrow(d), function(i) data.frame(level="species",
+                                                         name=d$speciesFixed[i],
+                                                         common=ifelse(!is.na(d$genus.common[i]), 
+                                                                       d$species.common[i], 
+                                                                       "Common name unknown"),
+                                                         inat_id=d$species.inat.ID[i],
+                                                         hex=d$hex[i],
+                                                         nspp=1,
+                                                         n_records=d$total_obs[i],
+                                                         n_users=d$user_obs[i],
+                                                         n_species=1))
+      
+      # function aggregates taxa list into parent clades
+      group_taxa <- function(data, groupings, level, max_col){
+            parents <- as.vector(unique(groupings[,1]))
+            lapply(parents, function(x){
+                  
+                  kids <- which(groupings[,1]==x)
+                  kids <- data[kids]
+                  kids <- kids[!sapply(kids, is.null)]
+                  
+                  weights_spp <- sapply(kids, function(x) x$nspp)
+                  weights_obs <- sapply(kids, function(x) x$nspp)
+                  
+                  color <- sapply(kids, function(x) x$hex) %>% col2rgb()
+                  color <- sapply(1:3, function(x) weighted.mean(color[x,], weights_spp))
+                  
+                  common <- d[d[,level] == x, paste0(level, ".common")][1]
+                  if(is.null(common)) common <- x
+                  
+                  inat_id <- d[d[,level] == x, paste0(level, ".inat.ID")][1]
+                  if(is.null(inat_id)) inat_id <- "NA"
+                  
+                  list(name=x,
+                       common=common,
+                       inat_id=inat_id,
+                       level=level, 
+                       hex=rgb(color[1], color[2], color[3], maxColorValue=max_col), # mcv/255 ratio controls fade to black
+                       nspp=sum(weights_spp),
+                       nobs=sum(weights_obs),
+                       children=kids)
+            })
       }
       
+      hierarchies <- list(linnean=c("root", "kingdom", "phylum", "class", "order", "family", "genus", "species"),
+                          simple=c("root", "category", "family", "species"))
       
-      
-      ### convert to JSON format
-      
-      # serialize to json
-      json <- toJSON(p, auto_unbox=T)
-      jsonn <- json
-      
-      # remove brackets from level names to match target format
-      jsonn <- gsub('\\[\\[', '[', jsonn)
-      jsonn <- gsub('\\]\\]', '\\]', jsonn)
-      jsonn <- gsub('\\],\\[', ',', jsonn)
-      
-      jsonn <- prettify(jsonn)
-      
-      # remove outermost brackets to match target format
-      jsonn <- stringr::str_trim(jsonn)
-      jsonn <- substr(jsonn, 2, nchar(jsonn)-1)
-      jsonn <- stringr::str_trim(jsonn)
-      
-      #write(jsonn, paste0("processed_data/",park_id,"_data/",park_id,"_taxonomy_", hierarchy, ".json"))
-      write(jsonn, paste0("d3_master/PORE_taxonomy_", hierarchy, ".json"))
+      for(hierarchy in names(hierarchies)){
+            # construct hierarchy
+            levels <- hierarchies[[hierarchy]]
+            p <- leaves
+            for(level in rev(levels)[2:length(levels)]){
+                  child_level <- levels[match(level, levels)+1]
+                  groupings <- distinct(d[,c(level, child_level)])
+                  groupings <- groupings[apply(groupings, 1, function(x) min(nchar(x))>0),]
+                  p <- group_taxa(p, groupings, level,
+                                  max_col=switch(hierarchy, "linnean"=400, "simple"=800))
+            }
+            
+            
+            
+            ### convert to JSON format
+            
+            # serialize to json
+            json <- toJSON(p, auto_unbox=T)
+            jsonn <- json
+            
+            # remove brackets from level names to match target format
+            jsonn <- gsub('\\[\\[', '[', jsonn)
+            jsonn <- gsub('\\]\\]', '\\]', jsonn)
+            jsonn <- gsub('\\],\\[', ',', jsonn)
+            
+            jsonn <- prettify(jsonn)
+            
+            # remove outermost brackets to match target format
+            jsonn <- stringr::str_trim(jsonn)
+            jsonn <- substr(jsonn, 2, nchar(jsonn)-1)
+            jsonn <- stringr::str_trim(jsonn)
+            
+            #write(jsonn, paste0("processed_data/",park_id,"_data/",park_id,"_taxonomy_", hierarchy, ".json"))
+            write(jsonn, paste0("d3_master/", park_id, "_taxonomy_", hierarchy, ".json"))
+      }
 }
-
--- code/user_stats.R
+++ code/user_stats.R
@@ -4,53 +4,53 @@ library(scales)
 #setwd("..")
 setwd("~/documents/inviz/taxorama")
 
-# load data
-park_id = "PORE" #Point Reyes is park of interest
-
-# load observations data
-d <- read.csv(paste0("processed_data/",park_id,"_data/",park_id,"_obs_tidy.csv"), header=T, stringsAsFactors=F) %>%
-      mutate(species=speciesFixed,
-             category=tolower(category),
-             root="life",
-             user=userNumber)
-
-# convert GMT to PST
-d$hour <- d$hour - 7
-d$hour[d$hour<0 & !is.na(d$hour)] <- d$hour[d$hour<0 & !is.na(d$hour)] + 24
-
-# assign obs to geographic bins (to address user cosmopolitanism)
-yrange <- range(d$decimalLatitude)
-xrange <- range(d$decimalLongitude)
-span <- max(diff(xrange), diff(yrange))
-resolution <- span / 5
-ra <- plyr::round_any
-d <- mutate(d,
-            xbin = as.integer(factor(ra(decimalLongitude, resolution, floor))),
-            ybin = as.integer(factor(ra(decimalLatitude, resolution, floor))),
-            geo_bin = paste(xbin, ybin))
-
-# compute user statistics and add to table
-# each user scored from 1-10 on # obs, spp, and visits
-jitter <- .01 # add a bit of noise to reduce overplotting
-u <- d %>%
-      group_by(userNumber) %>%
-      summarize(obs = n() + runif(1,0,jitter),
-                spp = length(unique(species)) + runif(1,0,jitter),
-                visits = length(unique(paste(year, month))) + runif(1,0,jitter),
-                locations = length(unique(geo_bin)) + runif(1,0,jitter)) %>%
-      mutate(obs = ecdf(obs)(obs),
-             spp = ecdf(spp)(spp),
-             visits = ecdf(visits)(visits),
-             locations = ecdf(locations)(locations)) %>%
-      mutate(user_obs = rescale(obs),
-             user_spp = rescale(spp),
-             user_visits = rescale(visits),
-             user_locations = rescale(locations),
-             user_hits = rescale(user_visits + user_locations)) %>%
-      select(userNumber, user_obs:user_locations)
-d <- left_join(d, u)
-
-d <- mutate(d, obs_id = 1:nrow(d))
-
-write.csv(d, "d3_master/PORE_obs_tidy.csv", row.names=F)
-
+for(park_id in c("PORE", "GOGA")){
+      
+      # load observations data
+      d <- read.csv(paste0("processed_data/", park_id, "_data/", park_id, "_obs_tidy.csv"), header=T, stringsAsFactors=F) %>%
+            mutate(species=speciesFixed,
+                   category=tolower(category),
+                   root="life",
+                   user=userNumber)
+      
+      # convert GMT to PST
+      if(!"hour" %in% names(d)) d$hour <- 12 # stopgap until Laura adds this field
+      d$hour <- d$hour - 7
+      d$hour[d$hour<0 & !is.na(d$hour)] <- d$hour[d$hour<0 & !is.na(d$hour)] + 24
+      
+      # assign obs to geographic bins (to address user cosmopolitanism)
+      yrange <- range(d$decimalLatitude)
+      xrange <- range(d$decimalLongitude)
+      span <- max(diff(xrange), diff(yrange))
+      resolution <- span / 5
+      ra <- plyr::round_any
+      d <- mutate(d,
+                  xbin = as.integer(factor(ra(decimalLongitude, resolution, floor))),
+                  ybin = as.integer(factor(ra(decimalLatitude, resolution, floor))),
+                  geo_bin = paste(xbin, ybin))
+      
+      # compute user statistics and add to table
+      # each user scored from 1-10 on # obs, spp, and visits
+      jitter <- .01 # add a bit of noise to reduce overplotting
+      u <- d %>%
+            group_by(userNumber) %>%
+            summarize(obs = n() + runif(1,0,jitter),
+                      spp = length(unique(species)) + runif(1,0,jitter),
+                      visits = length(unique(paste(year, month))) + runif(1,0,jitter),
+                      locations = length(unique(geo_bin)) + runif(1,0,jitter)) %>%
+            mutate(obs = ecdf(obs)(obs),
+                   spp = ecdf(spp)(spp),
+                   visits = ecdf(visits)(visits),
+                   locations = ecdf(locations)(locations)) %>%
+            mutate(user_obs = rescale(obs),
+                   user_spp = rescale(spp),
+                   user_visits = rescale(visits),
+                   user_locations = rescale(locations),
+                   user_hits = rescale(user_visits + user_locations)) %>%
+            select(userNumber, user_obs:user_locations)
+      d <- left_join(d, u)
+      
+      d <- mutate(d, obs_id = 1:nrow(d))
+      
+      write.csv(d, paste0("d3_master/", park_id, "_obs_tidy.csv"), row.names=F)
+}
